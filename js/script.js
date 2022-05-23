4a09cb2a7514a9ba2607048587ddb0a351f89f16
Katarzyna Banaszczyk <k.banaszczyk@hotmail.com>
2022-05-23 12:29:26 +0200

    added corrections to homework (form background, spaces in code)

--- js/script.js
+++ js/script.js
@@ -23,13 +23,10 @@
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-
         const amountElement = document.querySelector(".js-amount");
         const currencyElement = document.querySelector(".js-currency");
-
         const amount = +amountElement.value;
         const currency = currencyElement.value;
-       
         const result = calculateResult(amount, currency);
 
         updateResultText(amount, result, currency);
@@ -37,9 +34,8 @@
 
     const init = () => {
         const formElement = document.querySelector(".js-form");
-
         formElement.addEventListener("submit", onFormSubmit);
-        
+
         welcome();
     };
     init();
